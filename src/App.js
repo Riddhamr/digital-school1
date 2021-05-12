7920c03d7c13bdccdb9a905be0f111f5389178d1
Unknown <201801006@daiict.ac.in>
2021-05-12 13:38:16 +0530

    Fix: Routing problem check 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
     return (
-        <Router className="App">
-            <Route exact path="/">
-                <h1>Home</h1>
-            </Route>
-            <Route exact path="/about">
-                <h1>About</h1>
-            </Route>
+        <Router basename="/digital-school1" className="App">
+            <div className="menu">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/external">External</Link>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <h1>Home</h1>
+                </Route>
+                <Route exact path="/about">
+                    <h1>About</h1>
+                </Route>
+            </Switch>
         </Router>
     );
 }
