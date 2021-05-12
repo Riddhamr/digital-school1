c41b933950cb19209d28bd01c3d458ad2498cd1d
Unknown <201801006@daiict.ac.in>
2021-05-12 13:51:55 +0530

    Add: basic files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages";
 
 function App() {
     return (
         <Router basename="/digital-school1" className="App">
             <div className="menu">
                 <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/external">External</Link>
+                <Link to="/signin">Sign In</Link>
             </div>
             <Switch>
                 <Route exact path="/">
-                    <h1>Home</h1>
+                    <Home />
                 </Route>
-                <Route exact path="/about">
-                    <h1>About</h1>
+                <Route exact path="/signin">
+                    <h1>Sign In</h1>
                 </Route>
             </Switch>
         </Router>
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import video from "../../videos/video.mp4";
+import {
+    HeroBg,
+    HeroBtnWrapper,
+    HeroContainer,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    VideoBg,
+    ArrowForward,
+    ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElements";
+
+const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    };
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={video} type="video/mp4" />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual School Made Easy</HeroH1>
+
+                <HeroP>
+                    Sign up for a new account today and receive 7 day free trail
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button
+                        to="signup"
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                        primary="true"
+                        dark="true"
+                    >
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    );
+};
+
+export default HeroSection;
