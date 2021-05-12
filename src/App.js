c519704fd5b8a54c932cbb0569fb8f2e35d6d596
Unknown <201801006@daiict.ac.in>
2021-05-12 13:24:58 +0530

    Check Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <Router className="App">
+            <Route exact path="/">
+                <h1>Home</h1>
+            </Route>
+            <Route exact path="/about">
+                <h1>About</h1>
+            </Route>
+        </Router>
     );
 }
 
